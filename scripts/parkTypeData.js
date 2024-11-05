5d734df68d178a670944d719d12e44506307d2a0
Lena Man <manlena01@gmail.com>
2024-11-05 14:53:37 -0500

    park types

--- scripts/parkTypeData.js
+++ scripts/parkTypeData.js
@@ -1,18 +1,15 @@
-window.onload = init;
-
 const parkTypesArray = [
-    "National Park",
-    "National Monument",
-    "Recreation Area",
-    "Scenic Trail",
-    "Battlefield",
-    "Historic",
-    "Memorial",
-    "Preserve",
-    "Island",
-    "River",
-    "Seashore",
-    "Trail",
-    "Parkway"
+  "National Park",
+  "National Monument",
+  "Recreation Area",
+  "Scenic Trail",
+  "Battlefield",
+  "Historic",
+  "Memorial",
+  "Preserve",
+  "Island",
+  "River",
+  "Seashore",
+  "Trail",
+  "Parkway",
 ];
-
